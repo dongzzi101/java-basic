238d61f9d8cc0ae6285bd11dfb0c873f32d64fe3
dongzzi <dongzzi@gmail.com>
2025-03-25 15:45:22 +0900

    기본형 vs 참조형2 : - 대원칙 : 자바는 항상 변수의 값을 복사해서 대입한다. - 참조형의 경우 실제 사용하는 객체가 아니라 객체의 위치를 가르키는 참조값만 복사

---
+++ src/ref/VarChange2.java
@@ -0,0 +1,29 @@
+package ref;
+
+public class VarChange2 {
+
+    public static void main(String[] args) {
+        Data dataA = new Data();
+        dataA.value = 10;
+        Data dataB = dataA;
+
+        System.out.println("dataA 참조값 = " + dataA);
+        System.out.println("dataB 참조값 = " + dataB);
+        System.out.println("dataA.value = " + dataA.value); // 10
+        System.out.println("dataB.value = " + dataB.value); // 10
+
+        // data A 변경
+        dataA.value = 20;
+        System.out.println("data A 변경");
+        System.out.println("dataA.value = " + dataA.value); // 20
+        System.out.println("dataB.value = " + dataB.value); // 20
+
+        // data B 변경
+        dataB.value = 30;
+        System.out.println("data B 변경");
+        System.out.println("dataA.value = " + dataA.value); // 30
+        System.out.println("dataB.value = " + dataB.value); // 30
+
+
+    }
+}
