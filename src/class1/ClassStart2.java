f66ab62b133a37c731f41df80ee3de2cad700b08
dongzzi <dongzzi@gmail.com>
2025-03-25 11:29:17 +0900

    클래스가 필요한 이유 - 각각 만들면 관리하기가 어려워지고 코드가 복잡해진다.

---
+++ src/class1/ClassStart2.java
@@ -0,0 +1,16 @@
+package class1;
+
+public class ClassStart2 {
+
+    public static void main(String[] args) {
+        String[] studentsName = {"학생1", "학생2", "학생3"};
+        int[] studentsAge = {15, 13, 16};
+        int[] studentsGrade = {90, 80, 50};
+
+        for (int i = 0; i < studentsName.length; i++) {
+            System.out.println("이름: " + studentsName[i] + " 나이: " + studentsAge[i] + " 성적: " + studentsGrade[i]);
+        }
+
+
+    }
+}
