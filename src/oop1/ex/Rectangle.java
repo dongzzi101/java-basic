90930897d767088b7ecd4c2ea5c8e2410a366ab6
dongzzi <dongzzi101@gmail.com>
2025-03-26 11:40:55 +0900

    [문제와 풀이]

---
+++ src/oop1/ex/Rectangle.java
@@ -0,0 +1,21 @@
+package oop1.ex;
+
+public class Rectangle {
+
+    int width;
+    int height;
+
+
+    int calculatePerimeter() {
+        return (width + height) * 2;
+    }
+
+    boolean isSquare() {
+        return width == height;
+    }
+
+    int calculateArea() {
+        return width * height;
+    }
+
+}
