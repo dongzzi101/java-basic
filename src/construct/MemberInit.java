f8522bd2379d9276568777e7f85444cb45d81cfe
dongzzi <dongzzi101@gmail.com>
2025-03-26 12:33:07 +0900

    [this] : - 자기 자신의 인스턴스를 가르킴 - 멤버 변수와 메서드의 매게변수 이름이 같으면 어떻게 구분해야할까?     - 멤버 변수보다 매개변수가 코드 블럭의 더 안쪽에 있으면 매개변수가 우선순위를 가짐     - 멤버 변수에 접근하려면 앞에 this. 표시 → this는 인스턴스 자신의 참조값을 가리킴 - this 생략     - 변수를 찾을 때 가까운 지역변수(매개변수도 지역변수)를 먼저 찾고 없으면 그 다음으로 멤법 변수를 찾음 → 멤버 변수도 없으면 오류 발생

--- src/construct/MemberInit.java
+++ src/construct/MemberInit.java
@@ -6,4 +6,10 @@ public class MemberInit {
     int age;
     int grade;
 
+    void initMember(String name, int age, int grade) {
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+    }
+
 }
