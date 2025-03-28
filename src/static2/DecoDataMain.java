d801369c3d7b27ae54f52729833bc9105b16d5ea
dongzzi <dongzzi101@gmail.com>
2025-03-28 14:37:10 +0900

    [static 메서드3] : 용어정리 - 멤버 메서드의 종류     - 인스턴스 메서드 - static 없음     - 클래스 메서드 - static 있음         - 클래스 메서드, 정적 메서드, static 메서드로 부름 - import static 사용 가눙 - main() 메서드는 정적 메서드 → static만 호출 가능

--- src/static2/DecoDataMain.java
+++ src/static2/DecoDataMain.java
@@ -1,10 +1,15 @@
 package static2;
 
+import static static2.DecoData.staticCall;
+
 public class DecoDataMain {
 
     public static void main(String[] args) {
         System.out.println("1. 정적 호출");
-        DecoData.staticCall();
+        staticCall();
+        staticCall();
+        staticCall();
+        staticCall();
 
         System.out.println("2. 인스턴스 호출1");
         DecoData data1 = new DecoData();
@@ -14,7 +19,12 @@ public class DecoDataMain {
         DecoData data2 = new DecoData();
         data2.instanceCall();
 
-        DecoData.staticCall2(data1);
+        // 인스턴스를 통한 접근
+        DecoData data3 = new DecoData();
+        data3.staticCall();
+
+        // 클래스를 통한 접근
+        staticCall();
 
     }
 }
