c5c87c1dd2eb31da26ca52280df0083312b0fcbc
dongzzi <dongzzi101@gmail.com>
2025-03-28 16:40:34 +0900

    [final 변수와 상수1] : - 변수에 final 키워드가 붙으면 더는 값을 변결할 수 없다. - 초기화     - 생성자         - 객체 생성할 때 초기값 변경가능     - 필드         - 모든 인스턴스의 값이 동일해짐  → 같은 값을 사용하기 때문에 결과적으로 메모리 낭비 → static 영역으로 사용함 → static final - static final     - static 영역에 존재, final 키워드 사용해서 초기값이 변하지 않음     - 중복과 메모리 비효율 문제 해결

---
+++ src/final1/FieldInit.java
@@ -0,0 +1,13 @@
+package final1;
+
+public class FieldInit {
+
+    static final int CONST_VALUE = 10;
+
+    final int value = 10;
+
+    // 초기값 있으면 생성자에서 값 못 넣음
+    /*public FieldInit(int value) {
+        this.value = value;
+    }*/
+}
