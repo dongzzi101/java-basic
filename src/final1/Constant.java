d5c980ff46a17c3df038be858494543e1ca9f56f
dongzzi <dongzzi101@gmail.com>
2025-03-31 13:59:43 +0900

    [final 변수와 상수2] : 상수(constant) - 변하지 않고, 항상 일정한 값을 갖는 수

---
+++ src/final1/Constant.java
@@ -0,0 +1,14 @@
+package final1;
+
+public class Constant {
+
+    public static final double PI = 3.14159265358979323846;
+
+    public static final int HOURS_PER_DAY = 24;
+    public static final int MINUTES_PER_HOUR = 60;
+    public static final int SECONDS_PER_MINUTE = 60;
+
+    public static final int MAX_USERS = 2000;
+
+
+}
